977557b95abc8a5e20b63b7345e04f6d2dde7f7c
Pham Van Duong <83070538+frey-eth@users.noreply.github.com>
2024-06-17 09:58:37 +0700

    hehe

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -3,7 +3,7 @@ const Banner = () => {
     <div className='md:h-[100vh] h-[80vh] w-full bg-[url("/images/airdrop/bg.png")] bg-cover max-md:bg-contain max-md:bg-no-repeat flex items-center justify-center max-md:mt-10'>
       <div className="p-2">
         <div className="flex flex-col md:leading-[40px] leading-[24px] font-crotah text-[16px] md:text-[28px] text-blue-600 text-center py-3 px-6 backdrop-blur rounded-xl">
-          Proudly Launch on ZKsync
+          Launch on ZKsync
           <p className="md:text-[40px] text-[24px] text-white">
             Meow Meow Can Do Everything
           </p>
